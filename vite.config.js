916ef2f71b412c594f2559487ca9d70103798bfb
sethuka1220 <sethuka1220marin@gmail.com>
2024-08-23 11:25:54 +0900

    変更

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // GitHub Pages のデプロイ用に base を設定
+  base: process.env.NODE_ENV === "production" ? "/Shopping_List/" : "/", // GitHub Pages のデプロイ用に base を設定
 });
